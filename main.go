a2e993642ae201dce5c93a367a70fec112c94103
Ali <ali.naqvi.dev@gmail.com>
2022-08-02 07:20:23 -0500

    step 13 conditional

--- main.go
+++ main.go
@@ -17,11 +17,26 @@ func main() {
 		isHeistOn = false
 		fmt.Println("Ya'll didnt fool the guards")
 	}
+
 	fmt.Println("heist is on:", isHeistOn)
 	openedVault := rand.Intn(100)
+
 	if isHeistOn && openedVault >= 70 {
 		fmt.Println("Vaults open! Secure the bag!")
 	} else if isHeistOn && openedVault < 70 {
 		fmt.Println("The vault was so hard to open that when it did your soul was vaccuumed out and you died bro. Sorry.")
+		isHeistOn = false
 	}
+
+	leftSafely := rand.Intn(5)
+
+	if isHeistOn {
+		switch leftSafely{
+		case 0: 
+			isHeistOn = false
+			fmt.Println("The heist was not successful lil bruh.")
+		case
+		}
+	}
+
 }
